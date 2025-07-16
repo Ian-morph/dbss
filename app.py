b540e6b1d238160b7df97c4092d0a794ed0ccc04
ian-morph <ianlingben@gmail.com>
2025-07-16 11:13:10 +0800

    commit app telegram

--- app.py
+++ app.py
@@ -74,5 +74,9 @@ def prediction():
     pred = model.predict([[q]])
     return(render_template("prediction.html",r=pred))
 
+@app.route("/telegram",methods=["GET","POST"])
+def telegram():
+    return(render_template("telegram.html"))
+
 if __name__ == "__main__":
     app.run()
